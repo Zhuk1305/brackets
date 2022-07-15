0ba28429a2c05f279d39f5a68b423e4cea573806
Zhuk1305 <vaneczhuk@gmail.com>
2022-07-16 02:19:37 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+const delConfStr = (str, conf) => {
+  return str.replace(conf, "");
+};
+
+const recursionSearch = (str, confStr) => {
+  let res = str;
+
+  for (let i = 0; i < confStr.length; i++) {
+    if (res) {
+      res = delConfStr(res, confStr[i]);
+      if (res.length < str.length) {
+        res = recursionSearch(res, confStr);
+      }
+    }
+  }
+  return res;
+};
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const confStr = bracketsConfig.map((item) => item.join(""));
+  const answer = recursionSearch(str, confStr);
+
+  return !answer;
+};
